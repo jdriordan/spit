1b37af5e9a5d8e81630c76b558b477b0883bffd8
Danny Riordan <dannyriordan@gmail.com>
2014-07-06 14:29:17 +0100

    Initial Commit

---
+++ spit/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  spit
+//
+//  Created by Danny Riordan on 06/07/2014.
+//  Copyright (c) 2014 jdr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
